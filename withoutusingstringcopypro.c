6b48a3ef2eeed6d298c948f8021be2b698572eb7
Manikala959 <96293890+Manikala959@users.noreply.github.com>
2022-02-18 13:49:47 +0530

    Add files via upload

---
+++ withoutusingstringcopypro.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    char str1[100];
+	char str2[100];
+	scanf("%[^\n])s",str1);
+	int i;
+	for(i=0;str1[i]!='\0';i++)
+	{
+		str2[i]=str1[i];
+	}
+	 printf("%s",str2);	
+}
